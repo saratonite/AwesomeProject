cea2126a2e07f5bc7dbb53d143e97d8d756598ac
Sarath <sarath.hacks@gmail.com>
2017-06-22 17:55:42 +0530

    updates

--- index.android.js
+++ index.android.js
@@ -9,23 +9,52 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
+  TextInput,
+  Button,
   View
 } from 'react-native';
+
+import  Home  from './src/components/home';
 
 export default class AwesomeProject extends Component {
   constructor(props) {
 
     super(props)
 
-    this.state = {text:"Sarath"};
+    this.state = {name:"Sarath"};
 
   }
   render() {
+
+    let pic = {uri:'https://avatars1.githubusercontent.com/u/4659838?v=3&s=460'};
     return (
       <View style={styles.container}>
+        <Text>Sarath's Mobile App</Text>
+        <TextInput
+        style={{height: 40}}
+        onChangeText={(name) => this.setState({name})}
+        value={this.state.name}
+        placeholder="Name"
+      />
         <Text style={styles.welcome}>
-          Sarath
+          {this.state.name}
         </Text>
+        { /*<Image source={pic} style={{width: 200, height: 200}} /> */}
+
+        <Button
+          onPress={(e) => this.setState({name:''})}
+          title="CLEAR"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+         <Button
+          onPress={(e) => this.setState({name:'Sarath'})}
+          title="RESET"
+          color="#f56"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Home/>
       </View>
     );
   }
