54c55f67041726154240843d97e2df4913cb99a9
Sarath <saratonite@gmx.com>
2017-12-20 14:35:23 +0530

    :kiss: Packages updated

--- index.android.js
+++ index.android.js
@@ -15,7 +15,6 @@ import {
   View
 } from 'react-native';
 
-import  Home  from './src/components/home';
 
 export default class AwesomeProject extends Component {
   constructor(props) {
@@ -27,34 +26,10 @@ export default class AwesomeProject extends Component {
   }
   render() {
 
-    let pic = {uri:'https://avatars1.githubusercontent.com/u/4659838?v=3&s=460'};
     return (
       <View style={styles.container}>
-        <Text>Sarath's Mobile App</Text>
-        <TextInput
-        style={{height: 40}}
-        onChangeText={(name) => this.setState({name})}
-        value={this.state.name}
-        placeholder="Name"
-      />
-        <Text style={styles.welcome}>
-          {this.state.name}
-        </Text>
-        { /*<Image source={pic} style={{width: 200, height: 200}} /> */}
-
-        <Button
-          onPress={(e) => this.setState({name:''})}
-          title="CLEAR"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-         <Button
-          onPress={(e) => this.setState({name:'Sarath'})}
-          title="RESET"
-          color="#f56"
-          accessibilityLabel="Learn more about this purple button"
-        />
-        <Home/>
+        <Text style={styles.welcome}>Hello World</Text>
+        <Text style={styles.description}>Awesome react native app</Text>
       </View>
     );
   }
@@ -63,15 +38,17 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fefefe'
+    backgroundColor: '#f56',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 40,
     textAlign: 'center',
     margin: 10,
-    color:"#673AB7"
+    color:"#fff"
   },
-  instructions: {
+  description: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
